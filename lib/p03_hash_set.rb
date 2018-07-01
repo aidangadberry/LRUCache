0c71ef9c562e47816d8004979521bcb1cf7fd31e
Aidan Gadberry <agadberr@gmail.com>
2018-06-30 20:00:32 -0700

    HashSet

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -1,24 +1,34 @@
 class HashSet
   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    resize! if @count == num_buckets
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
-  def include?(key)
+  def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,14 @@ class HashSet
   end
 
   def resize!
+    temp_arr = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+
+    temp_arr.each do |bucket|
+      bucket.each do |el|
+        insert(el)
+      end
+    end
   end
 end
